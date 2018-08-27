c4d5d8817973b8fc6567681cfe8a7dca574fc19c
Alexius Academia <alexius.academia@gmail.com>
2018-08-27 21:46:47 +0800

    SteelTension - getter and setter

--- src/com/structuralengineering/rcbeam/properties/SteelTension.java
+++ src/com/structuralengineering/rcbeam/properties/SteelTension.java
@@ -1,4 +1,32 @@
 package com.structuralengineering.rcbeam.properties;
 
 public class SteelTension {
+  /**
+   * Total area of steel
+   * Default is in square millimeters
+   */
+  private double totalArea;
+
+  /** *******************************************
+   * Getters
+   ******************************************* */
+  public double getTotalArea(boolean inMetric) {
+    if (!inMetric) {
+      // Convert to english (square inches)
+      return totalArea / (Math.pow(25.4, 2));
+    } else {
+      return totalArea;
+    }
+  }
+
+  /** *******************************************
+   * Setters
+   ******************************************* */
+  public void setTotalArea(double totalArea, boolean inMetric) {
+    if (inMetric) {
+      this.totalArea = totalArea;
+    } else {
+      this.totalArea = totalArea * Math.pow(25.4, 2);
+    }
+  }
 }
