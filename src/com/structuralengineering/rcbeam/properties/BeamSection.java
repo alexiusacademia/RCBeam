6a2fd8eda90d901acb3930ccc9992ded800d494b
alexiusacademia <alexius.academia@gmail.com>
2018-09-03 19:15:18 +0800

    Worked on BeamSection class

--- src/com/structuralengineering/rcbeam/properties/BeamSection.java
+++ src/com/structuralengineering/rcbeam/properties/BeamSection.java
@@ -9,12 +9,28 @@ public class BeamSection {
     // Properties
     //
     // = = = = = = = = = = = = = = = = = = = = = =
-    /**
-     * Beam section definition.
-     */
-    private List<BeamSectionNode> section;
 
-    private SteelTension steelTension;
+    private List<BeamSectionNode> section;              // Beam section definition.
+    private SteelTension steelTension;                  // Tension steel property
+    private double effectiveDepth;                      // Depth of tension steel from concrete
+                                                        // extreme compression fiber in mm. (d)
+    private double dPrime;                              // Distance of compression steel to concrete
+                                                        // extreme compression fiber in mm. (d')
+    private double Ec;                                  // Concrete secant modulus in MPa. (Ec)
+    private double Es;                                  // Modulus of elasticity of steel in MPa. (Es)
+    private double modularRatio;                        // Modular ratio of steel to concrete. (n)
+    private double ductilityFactor;                     // Ductility factor, 𝜆
+    private double Lo;                                  // k1 * k2
+    private double ⲉo;                                  // ⲉo
+    private double β1;                                  // β1
+    private double fcPrime = 0;                         // Concrete yield strength MPa
+    private double fy = 0;                              // Steel yield strength MPa
+
+    // = = = = = = = = = = = = = = = = = = = = = =
+    //
+    // Constructors
+    //
+    // = = = = = = = = = = = = = = = = = = = = = =
 
     /**
      * Constructor with defined section nodes
@@ -54,12 +70,107 @@ public class BeamSection {
         return section;
     }
 
+    /**
+     * Gets the effective depth of beam.
+     * @return effectiveDepth (d)
+     */
+    public double getEffectiveDepth() {
+        return effectiveDepth;
+    }
+
+    /**
+     * Gets the d'
+     * @return d'
+     */
+    public double getdPrime() {
+        return dPrime;
+    }
+
+    /**
+     * Gets the concrete secant modulus.
+     * @return Ec in MPa.
+     */
+    public double getEc() {
+        return Ec;
+    }
+
+    /**
+     * Gets the modulus of elasticity of steel.
+     * @return Es in MPa.
+     */
+    public double getEs() {
+        return Es;
+    }
+
+    /**
+     * Gets the modular ratio.
+     * @return modularRatio
+     */
+    public double getModularRatio() {
+        return modularRatio;
+    }
+
     // = = = = = = = = = = = = = = = = = = = = = =
     //
     // Setters
     //
     // = = = = = = = = = = = = = = = = = = = = = =
 
+    /**
+     * Sets the fy.
+     * @param fy Steel yield strength.
+     */
+    public void setFy(double fy) {
+        this.fy = fy;
+    }
+
+    /**
+     * Sets the fc'
+     * @param fcPrime Concrete compressive strength
+     */
+    public void setFcPrime(double fcPrime) {
+        this.fcPrime = fcPrime;
+    }
+
+    /**
+     * Sets the effective depth of the beam.
+     * @param effectiveDepth
+     */
+    public void setEffectiveDepth(double effectiveDepth) {
+        this.effectiveDepth = effectiveDepth;
+    }
+
+    /**
+     * Sets the d'
+     * @param dPrime
+     */
+    public void setdPrime(double dPrime) {
+        this.dPrime = dPrime;
+    }
+
+    /**
+     * Sets the concrete secant modulus.
+     * @param ec Ec
+     */
+    public void setEc(double ec) {
+        this.Ec = ec;
+    }
+
+    /**
+     * Sets the modulus of elasticity of steel.
+     * @param es in MPa.
+     */
+    public void setEs(double es) {
+        Es = es;
+    }
+
+    /**
+     * Sets the modular ratio.
+     * @param modularRatio n
+     */
+    public void setModularRatio(double modularRatio) {
+        this.modularRatio = modularRatio;
+    }
 
     // = = = = = = = = = = = = = = = = = = = = = =
     //
@@ -70,7 +181,6 @@ public class BeamSection {
     /**
      * Add a single node to the beam section. Addition of nodes
      * must follow a clockwise notation.
-     *
      * @param node BeamSectionNode to be added.
      */
     public void addNode(BeamSectionNode node) {
@@ -78,8 +188,7 @@ public class BeamSection {
     }
 
     /**
-     * Remove a node at a specified index
-     *
+     * Remove a node at a specified index.
      * @param index The index in which the item is to be removed from.
      */
     public void removeNode(int index) {
@@ -98,5 +207,12 @@ public class BeamSection {
      */
     public void setSteelTension(double area, boolean inMetric) {
         this.steelTension.setTotalArea(area, inMetric);
+    }
+
+    /**
+     * Calculates the modular ratio.
+     */
+    private void calculateModularRatio() {
+        this.modularRatio = this.Es / this.Ec;
     }
 }
