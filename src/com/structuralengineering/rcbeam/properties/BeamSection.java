051eeb8d66f5995a002ab645975d6285d73c140e
alexiusacademia <alexius.academia@gmail.com>
2018-09-03 15:26:26 +0800

    Update BeamSection class

--- src/com/structuralengineering/rcbeam/properties/BeamSection.java
+++ src/com/structuralengineering/rcbeam/properties/BeamSection.java
@@ -4,72 +4,99 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BeamSection {
-  // = = = = = = = = = = = = = = = = = = = = = =
-  //
-  // Properties
-  //
-  // = = = = = = = = = = = = = = = = = = = = = =
-  /**
-   * Beam section definition.
-   */
-  private List<BeamSectionNode> section;
+    // = = = = = = = = = = = = = = = = = = = = = =
+    //
+    // Properties
+    //
+    // = = = = = = = = = = = = = = = = = = = = = =
+    /**
+     * Beam section definition.
+     */
+    private List<BeamSectionNode> section;
 
-  /**
-   * Constructor with defined section nodes
-   * @param nodes A list of x,y coordinate that defines the beam geometry
-   */
-  public BeamSection(List<BeamSectionNode> nodes) {
-    this.section = new ArrayList<>();
-    this.section = nodes;
-  }
+    private SteelTension steelTension;
 
-  /**
-   * Empty constructor
-   */
-  public BeamSection() {
-    this.section = new ArrayList<>();
-  }
+    /**
+     * Constructor with defined section nodes
+     *
+     * @param nodes A list of x,y coordinate that defines the beam geometry
+     */
+    public BeamSection(List<BeamSectionNode> nodes) {
+        // Initializations
+        this.section = new ArrayList<>();
+        this.steelTension = new SteelTension();
 
-  // = = = = = = = = = = = = = = = = = = = = = =
-  //
-  // Getters
-  //
-  // = = = = = = = = = = = = = = = = = = = = = =
-
-  /**
-   * Returns all the section nodes as ArrayList.
-   * @return section
-   */
-  public List<BeamSectionNode> getSection() {
-    return section;
-  }
-
-
-  // = = = = = = = = = = = = = = = = = = = = = =
-  //
-  // Methods
-  //
-  // = = = = = = = = = = = = = = = = = = = = = =
-
-  /**
-   * Add a single node to the beam section. Addition of nodes
-   * must follow a clockwise notation.
-   * @param node BeamSectionNode to be added.
-   */
-  public void addNode(BeamSectionNode node) {
-    this.section.add(node);
-  }
-
-  /**
-   * Remove a node at a specified index
-   * @param index The index in which the item is to be removed from.
-   */
-  public void removeNode(int index) {
-    // Check if index exist
-    if ((index - 1) < this.section.size()) {
-      // Index surely exist
-      // Remove the item
-      this.section.remove(index);
+        // Assignment
+        this.section = nodes;
     }
-  }
+
+    /**
+     * Empty constructor
+     */
+    public BeamSection() {
+        // Initializations
+        this.section = new ArrayList<>();
+        this.steelTension = new SteelTension();
+    }
+
+    // = = = = = = = = = = = = = = = = = = = = = =
+    //
+    // Getters
+    //
+    // = = = = = = = = = = = = = = = = = = = = = =
+
+    /**
+     * Returns all the section nodes as ArrayList.
+     *
+     * @return section
+     */
+    public List<BeamSectionNode> getSection() {
+        return section;
+    }
+
+    // = = = = = = = = = = = = = = = = = = = = = =
+    //
+    // Setters
+    //
+    // = = = = = = = = = = = = = = = = = = = = = =
+
+
+    // = = = = = = = = = = = = = = = = = = = = = =
+    //
+    // Methods
+    //
+    // = = = = = = = = = = = = = = = = = = = = = =
+
+    /**
+     * Add a single node to the beam section. Addition of nodes
+     * must follow a clockwise notation.
+     *
+     * @param node BeamSectionNode to be added.
+     */
+    public void addNode(BeamSectionNode node) {
+        this.section.add(node);
+    }
+
+    /**
+     * Remove a node at a specified index
+     *
+     * @param index The index in which the item is to be removed from.
+     */
+    public void removeNode(int index) {
+        // Check if index exist
+        if ((index - 1) < this.section.size()) {
+            // Index surely exist
+            // Remove the item
+            this.section.remove(index);
+        }
+    }
+
+    /**
+     * Sets the steel in tension area.
+     * @param area Area of steel.
+     * @param inMetric Metric preference.
+     */
+    public void setSteelTension(double area, boolean inMetric) {
+        this.steelTension.setTotalArea(area, inMetric);
+    }
 }
