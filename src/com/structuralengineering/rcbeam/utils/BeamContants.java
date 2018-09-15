46972ad73c78fbe41b2e83cfbd80b78835107555
Alexius Academia <alexius.academia@gmail.com>
2018-09-15 18:46:26 +0800

    Added the maximum concrete strain

--- src/com/structuralengineering/rcbeam/utils/BeamContants.java
+++ src/com/structuralengineering/rcbeam/utils/BeamContants.java
@@ -1,9 +1,16 @@
 package com.structuralengineering.rcbeam.utils;
 
 public class BeamContants {
-  /**
-   * Modulus of elasticity of steel.
-   * Unit in Mega Pascal (MPa).
-   */
-  public static double ES = 200000;
+    /**
+     * Modulus of elasticity of steel.
+     * Unit in Mega Pascal (MPa).
+     */
+    public static double ES = 200000;
+
+
+    /**
+     * Maximum strain in concrete
+     */
+    public static double ⲉcu = 0.003;
+
 }
