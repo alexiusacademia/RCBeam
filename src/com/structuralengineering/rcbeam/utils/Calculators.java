a171c705b501510c9d6bbc4b9ae515ed5c292dce
alexiusacademia <alexius.academia@gmail.com>
2018-09-17 17:18:57 +0800

    Fix bug in centroid calculation

--- src/com/structuralengineering/rcbeam/utils/Calculators.java
+++ src/com/structuralengineering/rcbeam/utils/Calculators.java
@@ -1,5 +1,6 @@
 package com.structuralengineering.rcbeam.utils;
 
+import com.structuralengineering.rcbeam.properties.BeamSection;
 import com.structuralengineering.rcbeam.properties.BeamSectionNode;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
@@ -39,6 +40,11 @@ public class Calculators {
         return area;
     }
 
+    /**
+     * Centroid of polygon from the top
+     * @param nodes Polygon definition
+     * @return centroid
+     */
     public static double calculateCentroidY(List<BeamSectionNode> nodes) {
         double kd = 0;
 
@@ -56,17 +62,14 @@ public class Calculators {
 
         double area = calculateArea(nodes);
 
-        double height = Math.abs(highestY(newNodes) - lowestY(newNodes));
-
-        int j;
-        for (int i = 0; i < n-1; i++) {
-            j = (i + 1) % n;
+        for (int i = 0; i < (n-1); i++) {
             kd += (newNodes.get(i).getY() + newNodes.get(i + 1).getY()) *
                     (newNodes.get(i).getX() * newNodes.get(i+1).getY() - newNodes.get(i+1).getX() * newNodes.get(i).getY());
         }
 
         kd = Math.abs(kd / (6 * area));
-        kd = Math.abs(height - kd);
+        kd = Math.abs(highestY(newNodes) - kd);
+
         return kd;
     }
 
@@ -102,6 +105,49 @@ public class Calculators {
         }
 
         return highest;
+    }
+
+    public static double getCentroidAboveAxis(double axisElevation, List<BeamSectionNode> nodes) {
+        // Hold the new nodes
+        List<BeamSectionNode> newNodes = new ArrayList<>();
+
+        int intersected = 0;
+        boolean isAbove = false;
+
+        double x1, x2, x3;
+        double y1, y2, y3;
+        y2 = axisElevation;
+
+        // Iterate to each node to look for intersection
+        for (int i = 1; i < nodes.size(); i++) {
+            if (intersected < 2) {
+                y1 = nodes.get(i-1).getY();
+                y3 = nodes.get(i).getY();
+                x1 = nodes.get(i-1).getX();
+                x3 = nodes.get(i).getX();
+                x2 = (y2 - y3) / (y1 - y3) * (x1 - x3) + x3;
+                if (y1 <= axisElevation && y3 > axisElevation) {
+                    // We got intersection
+                    newNodes.add(new BeamSectionNode(x2, y2));
+                    intersected++;
+                }
+                if (y1 >= axisElevation && y3 < axisElevation) {
+                    // We got intersection
+                    newNodes.add(new BeamSectionNode(x2, y2));
+                    intersected++;
+                }
+            }
+            if (nodes.get(i).getY() >= axisElevation) {
+                // Add only nodes that are above or equal to the cutting axis elevation
+                newNodes.add(nodes.get(i));
+            }
+        }
+
+        for (BeamSectionNode bsn : newNodes) {
+            System.out.println(bsn.getX() + ", " + bsn.getY());
+        }
+
+        return calculateCentroidY(newNodes);
     }
 
     public static double getAreaAboveAxis(double axisElevation, List<BeamSectionNode> nodes) {
