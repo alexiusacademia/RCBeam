a9650e36b16fd67d26c93cba57276bb581e57f5d
alexiusacademia <alexius.academia@gmail.com>
2018-09-18 22:32:24 +0800

    Update tester

--- src/com/structuralengineering/rcbeam/BeamAnalysisTester.java
+++ src/com/structuralengineering/rcbeam/BeamAnalysisTester.java
@@ -10,14 +10,14 @@ public class BeamAnalysisTester {
     public static void main(String[] args) {
         BeamSection bs = new BeamSection();
         // Rectangular
-        bs.addNode(new BeamSectionNode(0,0));
+        /*bs.addNode(new BeamSectionNode(0,0));
         bs.addNode(new BeamSectionNode(0,450));
         bs.addNode(new BeamSectionNode(300,450));
-        bs.addNode(new BeamSectionNode(300,0));
+        bs.addNode(new BeamSectionNode(300,0));*/
         // Triangular
-        /*bs.addNode(new BeamSectionNode(0,0));
-        bs.addNode(new BeamSectionNode(187.5,450));
-        bs.addNode(new BeamSectionNode(375, 0));*/
+        bs.addNode(new BeamSectionNode(0,0));
+        bs.addNode(new BeamSectionNode(150,600));
+        bs.addNode(new BeamSectionNode(300, 0));
         // T-Beam
         /*bs.addNode(new BeamSectionNode(0, 0));
         bs.addNode(new BeamSectionNode(0, 360));
@@ -33,33 +33,26 @@ public class BeamAnalysisTester {
         bs.addNode(new BeamSectionNode(300, 500));
         bs.addNode(new BeamSectionNode(300, 0));*/
 
-        bs.setFcPrime(21);
-        bs.setEffectiveDepth(400);
-        bs.setFy(275);
+        bs.setFcPrime(20);
+        bs.setEffectiveDepth(550);
+        bs.setFy(300);
 
         SteelTension st = new SteelTension();
       
-        st.setTotalArea(17000, Unit.METRIC);
+        st.setTotalArea(0, Unit.METRIC);
 
         bs.setSteelTension(st);
 
         BeamAnalyses analyses = new BeamAnalyses(bs);
 
-        BeamAnalysisResult limitAnalysis1 = analyses.beamCapacityAnalysis(StressDistribution.WHITNEY);
-        BeamAnalysisResult balancedAnalysis1 = analyses.balancedAnalysis(StressDistribution.WHITNEY);
-        double Mn = limitAnalysis1.getMomentC();
+        analyses.balancedAnalysis(StressDistribution.WHITNEY);
+        printString("Asb = " + analyses.getBalacedSteelTension());
 
-        printString("Mn (Whitney) = " + String.valueOf(Math.round(Mn * 1000) / 1000 / Math.pow(1000, 2)));
-        printString("kd (Whitney) = " + limitAnalysis1.getKd());
-        printString("Asb (Whitney) = " + analyses.getBalacedSteelTension());
+        BeamAnalysisResult limitState = analyses.beamCapacityAnalysis(StressDistribution.WHITNEY);
+        printString("Mn = " + limitState.getMomentC() / Math.pow(1000, 2));
 
-        BeamAnalysisResult limitAnalysis2 = analyses.beamCapacityAnalysis(StressDistribution.PARABOLIC);
-        BeamAnalysisResult balancedAnalysis2 = analyses.balancedAnalysis(StressDistribution.PARABOLIC);
-        Mn = limitAnalysis2.getMomentC();
-
-        printString("Mn (Parabolic) = " + String.valueOf(Math.round(Mn * 1000) / 1000 / Math.pow(1000, 2)));
-        printString("kd (Parabolic) = " + limitAnalysis2.getKd());
-        printString("Asb (Parabolic) = " + analyses.getBalacedSteelTension());
+        analyses.beforeCrackAnalysis();
+        printString("Asmin = " + analyses.getMinimumSteelTensionArea());
     }
 
     private static void printString(String str) {
